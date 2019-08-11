dd633bd929242093acf84ad77dca2b94c44501c6
Andrew Thiessen <squaids449@gmail.com>
2019-08-11 21:54:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  num = 0
+  while num < array.count do
+    arr = []
+    prod = array[num] ** 2
+    arr.unshift(prod)
+    num += 1
+    if num == array.count 
+      return arr
+    end
+  end
 end
